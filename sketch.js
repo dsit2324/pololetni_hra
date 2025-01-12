8223860e502fae6a62c90b0cfd9052a1fabf529e
dsit2324 <148313992+dsit2324@users.noreply.github.com>
2025-01-12 21:34:33 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
-let snowflakes = [];
-let snowflakeImage;
+const end = 10;
+let images = [];
+let collectImage;
 let backgroundImage;
 let movingImage;
-let dingSound;
+let collisionSound;
 let pokusny;
 let music; // Přidána proměnná pro hudbu
+let gameOver = false; // Proměnná pro sledování stavu hry
 
 class Rectangle {
   constructor(x, y) {
@@ -34,13 +36,13 @@ class Rectangle {
     this.y = constrain(this.y + dy * this.speed, 0, height - this.height);
   }
 
-  detectCollision(snowflake) {
+  detectCollision(image) {
     return collideRectCircle(this.x, this.y, this.width, this.height, 
-      snowflake.x, snowflake.y, snowflake.size);
+      image.x, image.y, image.size);
   }
 }
 
-class Snowflake {
+class Obrazek {
   constructor() {
     this.x = random(width);
     this.y = 0;
@@ -50,9 +52,9 @@ class Snowflake {
   }
 
   draw() {
-    if (snowflakeImage) {
+    if (collectImage) {
       imageMode(CENTER);
-      image(snowflakeImage, this.x, this.y, this.size * 7.5, this.size * 7.5);
+      image(collectImage, this.x, this.y, this.size * 7.5, this.size * 7.5);
     } else {
       fill(this.color);
       circle(this.x, this.y, this.size);
@@ -67,8 +69,8 @@ class Snowflake {
 function preload() {
   // Načtení obrázku a zvuku
   movingImage = loadImage('walter.png');
-  snowflakeImage = loadImage('meth.webp');
-  dingSound = loadSound('kolize.mp3');
+  collectImage = loadImage('meth.webp');
+  collisionSound = loadSound('kolize.mp3');
   backgroundImage = loadImage('van.webp');
   music = loadSound('saul.mp3'); // Načtení hudby
 }
@@ -76,11 +78,11 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pokusny = new Rectangle(500, 300);
-  snowflakes.push(new Snowflake());
+  images.push(new Obrazek());
 
   // Spuštění hudby na pozadí
   if (music && music.isLoaded()) {
-    music.setVolume(0.5);
+    music.setVolume(0.1);
     music.loop();
   } else {
     console.error("Hudba se nenačetla správně.");
@@ -88,6 +90,29 @@ function setup() {
 }
 
 function draw() {
+  // Pokud hra skončila, nezobrazuje nic jiného
+  if (gameOver) {
+    // Nastavení barvy pro obdélník (např. červený okraj a průhledný vnitřek)
+    fill(0); // Poloprůhledný červený
+    stroke(255);  // Bílý okraj
+
+    // Vykreslení obdélníku kolem textu
+    let textWidthValue = textWidth("You have won!") + 40; // Šířka textu + okraje
+    let textHeightValue = 64 + 20; // Výška textu + okraje
+    let rectX = width / 2 - textWidthValue / 2;
+    let rectY = height / 2 - textHeightValue / 2;
+
+    rect(rectX, rectY, textWidthValue, textHeightValue); // Vykreslí obdélník
+
+    // Zobrazení textu "HRA SKONČILA"
+    fill(255); // Bílá barva pro text
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("You have won!", width / 2, height / 2);
+
+    return; // Zastaví vykreslování
+  }
+
   if (backgroundImage) {
     imageMode(CORNER);
     image(backgroundImage, 0, 0, width, height);
@@ -95,44 +120,60 @@ function draw() {
     background(20);
   }
 
-  // Aktualizace sněhové vločky
-  for (let i = 0; i < snowflakes.length; i++) {
-    snowflakes[i].update();
-    snowflakes[i].draw();
-    if (pokusny.detectCollision(snowflakes[i])) {
+  // Aktualizace methu
+  for (let i = 0; i < images.length; i++) {
+    images[i].update();
+    images[i].draw();
+    if (pokusny.detectCollision(images[i])) {
       pokusny.points++;
       console.log(pokusny.points);
-      if (dingSound && dingSound.isLoaded())
-        dingSound.setVolume(5.0);
-        dingSound.play();
 
-      snowflakes.splice(i, 1);
-      snowflakes.push(new Snowflake());
+      // Kontrola, zda hráč nasbíral 20 bodů
+      if (pokusny.points >= end) {
+        gameOver = true; // Pokud ano, hra skončí
+      }
+
+      if (collisionSound && collisionSound.isLoaded()) {
+        collisionSound.setVolume(5.0);
+        collisionSound.play();
+      }
+
+      images.splice(i, 1); // Odstranění methu, ktery byl sebrán
+      images.push(new Obrazek()); // Vytvoření nového methu
     }
-    if (snowflakes[i].y > height + 20) {
-      snowflakes.splice(i, 1);
-      snowflakes.push(new Snowflake());
+    if (images[i].y > height + 20) {
+      images.splice(i, 1);  // Pokud meth spadne mimo obrazovku, odstraníme ji
+      images.push(new Obrazek());
     }
   }
 
-  if (keyIsDown(LEFT_ARROW)) {
-    pokusny.move(-1, 0);
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    pokusny.move(1, 0);
-  }
-  if (keyIsDown(UP_ARROW)) {
-    pokusny.move(0, -1);
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    pokusny.move(0, 1);
+  // Kontrola pohybu pouze, pokud hra neskončila
+  if (!gameOver) {
+    if (keyIsDown(LEFT_ARROW)) {
+      pokusny.move(-1, 0);
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      pokusny.move(1, 0);
+    }
+    if (keyIsDown(UP_ARROW)) {
+      pokusny.move(0, -1);
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      pokusny.move(0, 1);
+    }
   }
 
   // Zobrazení skóre v pravém horním rohu
   fill(255);
   textSize(32);
   textAlign(LEFT, TOP);
-  text("Skóre: " + pokusny.points, 20, 20);
+  text("Score: " + pokusny.points, 20, 20);
+
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, TOP);
+  text("The goal of the game is to collect "  + end + " meths.", windowWidth / 2, 20);
+
 
   pokusny.draw();
 }
